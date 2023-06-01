c666bdc84f942f4e36ee7951ab2562bbcc556ce3
Thomas Williams <orion5williams@gmail.com>
2023-05-31 21:35:42 -0500

    front-end tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <!-- Javascript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     </head>
-    <body class="antialiased">
+    <body class="antialiased ">
         <div class="card">
                 <h1>Welcome to the Ciguatera Reporting site</h1>
         </div>
